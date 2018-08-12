a8676af3a1300e18833a47d6ad69c754e1de2855
Alexander Slesarenko <avslesarenko@gmail.com>
2018-08-12 15:36:14 +0300

    NonEquals primitive added

--- core/src/main/scala/scalan/primitives/Equal.scala
+++ core/src/main/scala/scalan/primitives/Equal.scala
@@ -4,20 +4,28 @@ import scalan.{Base, Scalan}
 
 trait Equal extends Base { self: Scalan =>
   case class Equals[A: Elem]() extends BinOp[A, Boolean]("==", equalValues[A](_, _))
+  case class NotEquals[A: Elem]() extends BinOp[A, Boolean]("!=", !equalValues[A](_, _))
 
   def equalValues[A](x: Any, y: Any)(implicit eA: Elem[A]) = x == y
 
   implicit class EqualOps[A](x: Rep[A]) {
     implicit val eA = x.elem
     def ===(y: Rep[A]): Rep[Boolean] = Equals[A].apply(x, y)
-    def !==(y: Rep[A]): Rep[Boolean] = !Equals[A].apply(x, y)
+    def !==(y: Rep[A]): Rep[Boolean] = NotEquals[A].apply(x, y)
   }
   override def rewriteDef[T](d: Def[T]) = d match {
+    // rules for Equals
     case ApplyBinOp(_: Equals[_], x, y) if x == y => true
     case ApplyBinOp(_: Equals[_], x, Def(Const(b: Boolean))) if x.elem == BooleanElement =>
       if (b) x else !x.asRep[Boolean]
     case ApplyBinOp(_: Equals[_], Def(Const(b: Boolean)), x) if x.elem == BooleanElement =>
       if (b) x else !x.asRep[Boolean]
+    // rules for NotEquals
+    case ApplyBinOp(_: NotEquals[_], x, y) if x == y => false
+    case ApplyBinOp(_: NotEquals[_], x, Def(Const(b: Boolean))) if x.elem == BooleanElement =>
+      if (b) !x.asRep[Boolean] else x
+    case ApplyBinOp(_: NotEquals[_], Def(Const(b: Boolean)), x) if x.elem == BooleanElement =>
+      if (b) !x.asRep[Boolean] else x
     case _ => super.rewriteDef(d)
   }
 }
