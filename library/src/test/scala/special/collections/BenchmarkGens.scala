2f9bed3707ed60c26e727c2874920147fc8c2622
Alexander Slesarenko <avslesarenko@gmail.com>
2019-07-04 09:46:50 +0300

    MapBenchmark.scala added

--- library/src/test/scala/special/collections/BenchmarkGens.scala
+++ library/src/test/scala/special/collections/BenchmarkGens.scala
@@ -3,9 +3,11 @@ package special.collections
 import org.scalameter.api.Gen
 
 trait BenchmarkGens extends CollGens {
-  val sizes = Gen.exponential("size")(10, 100000, 10)
+  val maxSize = 100000
 
-  val ranges = for { size <- sizes } yield (0 until size, 100000 / size)
+  val sizes = Gen.exponential("size")(10, maxSize, 10)
+
+  val ranges = for { size <- sizes } yield (0 until size, maxSize / size)
 
   val arrays = ranges.map { case (r, i) => (r.toArray, i) }
 
