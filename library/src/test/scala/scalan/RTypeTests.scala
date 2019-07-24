ba76c1c0e621916916d2fbc0bad29fb24e2dea25
Eugene Gostkin <gostkin.em@phystech.edu>
2019-07-24 05:36:30 +0300

    Add coverage test for FullTypeGenerator

--- library/src/test/scala/scalan/RTypeTests.scala
+++ library/src/test/scala/scalan/RTypeTests.scala
@@ -4,6 +4,50 @@ import org.scalacheck.Gen
 import org.scalatest.{Matchers, PropSpec}
 import org.scalatest.prop.PropertyChecks
 
+import spire.syntax.all._
+
+trait RTypeGenCoverageChecker {
+  def encounter(item: RType[_]): Unit
+  def isCovered(depth: Int): Boolean
+}
+
+class RTypeFullGenCoverageChecker extends RTypeGenCoverageChecker {
+  import RType._
+
+  private var usedPlaces: Set[(String, Int)] = Set.empty
+
+  override def encounter(item: RType[_]): Unit = {
+    parseValue(item)
+  }
+
+  override def isCovered(depth: Int): Boolean = {
+    val names = Seq("PrimitiveType", "PairType", "ArrayType", "StringType")
+    cfor(0)(_ < depth, _ + 1) { i =>
+      for (name <- names)
+        if (!usedPlaces.contains((name, i)))
+          false
+    }
+    true
+  }
+
+  private def attachResult(item: String, depth: Int) = {
+    val newItem = (item, depth)
+    usedPlaces = usedPlaces + newItem
+  }
+
+  private def parseValue(item: RType[_], depth: Int = 0): Unit = item match {
+    case prim: PrimitiveType[a] => attachResult("PrimitiveType", depth)
+    case pair: PairType[a, b]@unchecked =>
+      attachResult("PairType", depth)
+      parseValue(pair.tFst, depth + 1)
+      parseValue(pair.tSnd, depth + 1)
+    case array: ArrayType[a] =>
+      attachResult("ArrayType", depth)
+      parseValue(array.tA, depth + 1)
+    case (stringType: RType[String]@unchecked) => attachResult("StringType", depth)
+    case _ => throw new RuntimeException(s"Unknown generated RType: ${item}")
+  }
+}
 
 class RTypeTests extends PropSpec with PropertyChecks with Matchers with RTypeGens {
   testSuite =>
@@ -11,12 +55,13 @@ class RTypeTests extends PropSpec with PropertyChecks with Matchers with RTypeGe
   import Gen._
   import RType._
 
-  /*
-  property("RType show functionality") {
-    val minSuccess = MinSuccessful(30)
-    forAll(fullDataTypeGen, minSuccess) { t: RType[_] =>
-      println(t)
+  property("RType generate value by type") {
+    val minSuccess = MinSuccessful(300)
+
+    val counter = new RTypeFullGenCoverageChecker()
+    forAll(getFullTypeGen(3), minSuccess) { t: RType[_] =>
+      counter.encounter(t)
     }
+    counter.isCovered(3) shouldBe true
   }
-  */
 }
