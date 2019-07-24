14b5ca12d1a139671cfa3ab957e33f913fc2a00f
Eugene Gostkin <gostkin.em@phystech.edu>
2019-07-24 05:00:17 +0300

    Enhance generators

--- library/src/test/scala/scalan/RTypeGens.scala
+++ library/src/test/scala/scalan/RTypeGens.scala
@@ -6,23 +6,30 @@ trait RTypeGens {
   import Gen._
   import RType._
 
-  val primitiveNonUnitTypeGen = Gen.oneOf[RType[_]](BooleanType, ByteType, ShortType,
+  /*
+   * Primitive and simple type generators
+   */
+
+  val primitiveTypeGen = Gen.oneOf[RType[_]](BooleanType, ByteType, ShortType,
     IntType, LongType, CharType, FloatType, DoubleType)
 
-  val primitiveTypeGen = Gen.oneOf[RType[_]](primitiveNonUnitTypeGen, UnitType)
+  val primitiveTypeWithUnitGen = Gen.oneOf[RType[_]](primitiveTypeGen, UnitType)
 
   val dataTypeGen = Gen.oneOf[RType[_]](primitiveTypeGen, StringType)
+
+  /*
+   */
 
   def pairTypeGenFinal(itemGenLeft: Gen[RType[_]], itemGenRight: Gen[RType[_]]): Gen[PairType[_, _]] = {
     for { left <- itemGenLeft; right <- itemGenRight } yield new PairType(left, right)
   }
 
-  def pairTypeGen(depth: Int = 1, itemGen: Gen[RType[_]]): Gen[PairType[_, _]] = depth match {
+  def pairTypeGen(itemGen: Gen[RType[_]], depth: Int = 1): Gen[PairType[_, _]] = depth match {
     case 1 =>
       pairTypeGenFinal(itemGen, itemGen)
     case _ =>
-      val lg = pairTypeGen(depth - 1, itemGen)
-      val rg = pairTypeGen(depth - 1, itemGen)
+      val lg = pairTypeGen(itemGen, depth - 1)
+      val rg = pairTypeGen(itemGen, depth - 1)
       Gen.oneOf(
         pairTypeGenFinal(itemGen, itemGen),
         pairTypeGenFinal(lg, itemGen),
@@ -31,24 +38,43 @@ trait RTypeGens {
       )
   }
 
-  def getPairGen(depth: Int = 4, gn: Gen[RType[_]]) = pairTypeGen(depth, gn)
-  def getPairDataTypeGen(depth: Int = 4) = pairTypeGen(depth, dataTypeGen)
+  def getPairAnyTypeGen(gn: Gen[RType[_]], depth: Int = 4): Gen[RType[_]] = pairTypeGen(gn, depth)
 
   def arrayTypeGenFinal(itemGen: Gen[RType[_]]): Gen[ArrayType[_]] = {
     for { item <- itemGen } yield new ArrayType(item)
   }
 
-  def arrayTypeGen(depth: Int = 1, itemGen: Gen[RType[_]]): Gen[ArrayType[_]] = depth match {
+  def arrayTypeGen(itemGen: Gen[RType[_]], depth: Int = 1): Gen[ArrayType[_]] = depth match {
     case 1 =>
       arrayTypeGenFinal(itemGen)
     case _ =>
       Gen.oneOf(
         arrayTypeGenFinal(itemGen),
-        arrayTypeGenFinal(arrayTypeGen(depth - 1, itemGen))
+        arrayTypeGenFinal(arrayTypeGen(itemGen, depth - 1))
       )
   }
 
-  val getArrayPrimitiveTypeGen = arrayTypeGen(4, primitiveTypeGen)
-  val arrayDataTypeGen = arrayTypeGen(4, Gen.oneOf(dataTypeGen, getPairDataTypeGen()))
-  val fullDataTypeGen = Gen.oneOf(dataTypeGen, getPairDataTypeGen(), arrayDataTypeGen)
+  def getArrayAnyTypeGen(gn: Gen[RType[_]], depth: Int = 4): Gen[RType[_]] = arrayTypeGen(gn, depth)
+
+  /*
+   * Full type generators
+   */
+  def getFullTypeGen(depth: Int = 4): Gen[RType[_]] = depth match {
+    case 1 => Gen.oneOf(dataTypeGen, getPairAnyTypeGen(dataTypeGen, 1), getArrayAnyTypeGen(dataTypeGen, 1))
+    case _ =>
+      Gen.oneOf(dataTypeGen, getPairAnyTypeGen(getFullTypeGen(depth - 1), 1), getArrayAnyTypeGen(getFullTypeGen(depth - 1), 1))
+  }
+
+  val fullTypeGen = getFullTypeGen(3)
+
+  def getArrayPrimitiveTypeGen(depth: Int = 4) = getArrayAnyTypeGen(primitiveTypeGen, depth)
+  def getArrayTypeGen(depth: Int = 4) = getArrayAnyTypeGen(getFullTypeGen(depth - 1), 1)
+
+  def getPairPrimitiveTypeGen(depth: Int = 4) = getPairAnyTypeGen(primitiveTypeGen, depth)
+  def getPairTypeGen(depth: Int = 4) = getPairAnyTypeGen(getFullTypeGen(depth - 1), 1)
+
+  val arrayPrimitiveTypeGen = getArrayPrimitiveTypeGen()
+  val arrayTypeGen = getArrayTypeGen()
+  val pairPrimitiveTypeGen = getPairPrimitiveTypeGen()
+  val pairTypeGen = getPairTypeGen()
 }
