f271baf1719ebf409e695b37f5620a71e437bb08
Eugene Gostkin <gostkin.em@phystech.edu>
2019-07-26 17:43:21 +0300

    Implement cloning

--- library/src/test/scala/scalan/RTypeTests.scala
+++ library/src/test/scala/scalan/RTypeTests.scala
@@ -42,13 +42,40 @@ class RTypeTests extends PropSpec with PropertyChecks with Matchers with RTypeGe
   property("RType clone value") {
     import scala.runtime.ScalaRunTime._
     val minSuccess = MinSuccessful(30)
-    def checkCopy[T](value: T)(implicit tA: RType[T]) = {
-      val copy: T = RTypeUtil.clone(tA, value)
-      copy == value shouldBe true
+    def checkCopy[T](value: T)(implicit tA: RType[_]): Unit = {
+      var copy: T = RTypeUtil.clone(tA, value).asInstanceOf[T]
+
+      def deepEqualityChecker[A](value: A, copy: A)(implicit tA: RType[_]): Boolean = tA match {
+        case arrayType: ArrayType[a] =>
+          val valInstance = value.asInstanceOf[Array[a]]
+          val copyInstance = copy.asInstanceOf[Array[a]]
+          valInstance.length shouldBe copyInstance.length
+          cfor(0)(_ < valInstance.length, _ + 1) { i =>
+            if (!deepEqualityChecker(valInstance(i), copyInstance(i))(arrayType.tA))
+              return false
+          }
+          true
+        case prim: PrimitiveType[a] =>
+          copy == value
+        case pairType: PairType[a, b] =>
+          val valInstance = value.asInstanceOf[Tuple2[a, b]]
+          val copyInstance = copy.asInstanceOf[Tuple2[a, b]]
+          deepEqualityChecker(valInstance._1, copyInstance._1)(pairType.tFst) && deepEqualityChecker(valInstance._2, copyInstance._2)(pairType.tSnd)
+        case (string: RType[String]@unchecked) =>
+          copy == value
+        case _ => copy == value
+      }
+      deepEqualityChecker(copy, value)(tA) shouldBe true
+      val res = tA match {
+        case prim: PrimitiveType[a] => true
+        case _ => !(copy.asInstanceOf[AnyRef] eq value.asInstanceOf[AnyRef])
+      }
+      res shouldBe true
     }
-    forAll(getFullTypeGen(3), minSuccess) { tA: RType[_] =>
-      forAll(rtypeValueGen(tA), MinSuccessful(4)) { value =>
-        checkCopy(value)(tA)
+
+    forAll(getFullTypeGen(typeGenerationDepth), MinSuccessful(300)) { tA: RType[_] =>
+      forAll(rtypeValueGen(tA)) { value =>
+        checkCopy(value.asInstanceOf[Any])(tA)
       }
     }
   }
