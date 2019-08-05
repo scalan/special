0044c660411f7af3658c8b943b1ffe92f8a35e44
Eugene Gostkin <gostkin.em@phystech.edu>
2019-08-05 03:44:42 +0300

    Add Coll and Option support

--- library-impl/src/main/scala/scalan/RTypeUtil.scala
+++ library-impl/src/main/scala/scalan/RTypeUtil.scala
@@ -5,8 +5,9 @@ import spire.syntax.trig.trigOps
 
 object RTypeUtil {
   import scalan.RType._
+  import special.collection._
 
-  def clone[T](t: RType[_], value: T): Any = t match {
+  def clone[T](t: RType[_], value: T): T = t match {
     case prim: PrimitiveType[a] => value
     case arrayType: ArrayType[a] =>
       val array = value.asInstanceOf[Array[a]]
@@ -14,13 +15,25 @@ object RTypeUtil {
       cfor(0)(_ < array.length, _ + 1) { i =>
         copy(i) = clone(arrayType.tA, array(i)).asInstanceOf[a]
       }
-      copy
+      copy.asInstanceOf[T]
     case pairType: PairType[a, b] =>
-      val instanced = value.asInstanceOf[Tuple2[a, b]]
-      return (clone(pairType.tFst, instanced._1), clone(pairType.tSnd, instanced._2))
-    case (stringType: RType[String]@unchecked) =>
+      val pair = value.asInstanceOf[Tuple2[a, b]]
+      return (clone(pairType.tFst, pair._1), clone(pairType.tSnd, pair._2)).asInstanceOf[T]
+    case optionType: OptionType[a] =>
+      val option = value.asInstanceOf[Option[a]]
+      val res = if (option.isDefined) Some(clone(optionType.tA, option.get)) else None
+      res.asInstanceOf[T]
+    case replCollType: ReplCollType[a] =>
+      val coll = value.asInstanceOf[ReplColl[a]]
+      val cloned = clone(replCollType.tItem, coll.value)
+      (new CReplColl(cloned, coll.length)(replCollType.tItem)).asInstanceOf[T]
+    case collType: CollType[a] =>
+      val coll = value.asInstanceOf[Coll[a]]
+      val cloned = clone(new ArrayType(collType.tItem), coll.toArray)
+      coll.builder.fromArray(cloned)(collType.tItem).asInstanceOf[T]
+    case StringType =>
       val arr = value.asInstanceOf[String].toArray
-      arr.mkString
-    case _ => throw new NotImplementedError("Not supported")
+      arr.mkString.asInstanceOf[T]
+    case _ => throw new RuntimeException(s"Can't clone ${t}.")
   }
 }
--- library/src/test/scala/scalan/RTypeTests.scala
+++ library/src/test/scala/scalan/RTypeTests.scala
@@ -41,9 +41,10 @@ class RTypeTests extends PropSpec with PropertyChecks with Matchers with RTypeGe
 
   property("RType clone value") {
     import scala.runtime.ScalaRunTime._
-    val minSuccess = MinSuccessful(30)
-    def checkCopy[T](value: T)(implicit tA: RType[_]): Unit = {
+    def checkCopy[T](value: T, tA: RType[_]): Unit = {
       var copy: T = RTypeUtil.clone(tA, value).asInstanceOf[T]
+      println(stringOf(value))
+      println(stringOf(copy))
 
       def deepEqualityChecker[A](value: A, copy: A)(implicit tA: RType[_]): Boolean = tA match {
         case arrayType: ArrayType[a] =>
@@ -61,21 +62,24 @@ class RTypeTests extends PropSpec with PropertyChecks with Matchers with RTypeGe
           val valInstance = value.asInstanceOf[Tuple2[a, b]]
           val copyInstance = copy.asInstanceOf[Tuple2[a, b]]
           deepEqualityChecker(valInstance._1, copyInstance._1)(pairType.tFst) && deepEqualityChecker(valInstance._2, copyInstance._2)(pairType.tSnd)
-        case (string: RType[String]@unchecked) =>
+        case StringType =>
           copy == value
         case _ => copy == value
       }
       deepEqualityChecker(copy, value)(tA) shouldBe true
       val res = tA match {
         case prim: PrimitiveType[a] => true
+        case optionType: OptionType[a] if value.asInstanceOf[Option[a]].isEmpty => true
         case _ => !(copy.asInstanceOf[AnyRef] eq value.asInstanceOf[AnyRef])
       }
       res shouldBe true
     }
 
-    forAll(getFullTypeGen(typeGenerationDepth), MinSuccessful(300)) { tA: RType[_] =>
-      forAll(rtypeValueGen(tA)) { value =>
-        checkCopy(value.asInstanceOf[Any])(tA)
+    val minSuccess = MinSuccessful(PosInt.from(coverageThreshold).get)
+    forAll(extendedTypeGen(typeGenDepth), minSuccess) { t: RType[_] =>
+      forAll(extendedValueGen(t)) { value =>
+        println(t)
+        checkCopy(value, t)
       }
     }
   }
